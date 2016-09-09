bc218fcbfe2a41787fd87f8232ff9eec4c5786bb
Henry Ammermann <hintsss@gmail.com>
2016-09-09 04:12:39 -0700

    now to just update Botss.java

--- src/main/java/pw/hintss/botss/BotMain.java
+++ src/main/java/pw/hintss/botss/BotMain.java
@@ -1,11 +1,23 @@
 package pw.hintss.botss;
 
+import org.pircbotx.Configuration;
+import org.pircbotx.PircBotX;
+
 public class BotMain {
     public static void main(String[] args) throws Exception {
-        Botss bot = new Botss();
-        bot.setVerbose(true);
-        bot.connect("irc.subluminal.net");
-        bot.setMode(bot.getNick(), "+B");
-        bot.joinChannel("#programming");
+        Configuration config = new Configuration.Builder()
+                .setAutoNickChange(true)
+                .setAutoReconnect(true)
+                .setLogin("botss")
+                .setName("botss")
+                .setOnJoinWhoEnabled(true)
+                .setRealName("botss, by hintss")
+                .setVersion("botss by hintss, using pircbotx 2.1, https://github.com/hintss/botss")
+                .addServer("irc.subluminal.net")
+                .addAutoJoinChannel("#programming")
+                .buildConfiguration();
+
+        PircBotX bot = new PircBotX(config);
+        bot.startBot();
     }
 }
